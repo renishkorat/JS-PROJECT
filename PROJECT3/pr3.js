bbdd562e7228b11aaca54d7185c3b5a3139d1029
Renish Korat <koratrenish350@gmail.com>
2026-08-06 11:05:51 +0530

    update

--- PROJECT3/pr3.js
+++ PROJECT3/pr3.js
@@ -7,6 +7,7 @@
 // }
 // let numbers: number[] = [10, 20, 30, 40, 70];
 // printArray(numbers);
+
 // 2. Find Sum of Array
 // function sumArray(arr: number[]): number {
 //     let sum: number = 0;
@@ -17,6 +18,7 @@
 // }
 // let number: number[] = [10, 20, 30, 40, 50];
 // console.log("sum =", sumArray(number));
+
 // 3. Find Average of Array
 // function avarageArray(arr: number[]): number {
 //     let sum: number = 0;
@@ -27,6 +29,7 @@
 // }
 // let number: number[] = [10, 4, 50, 60, 100];
 // console.log("Average =", avarageArray(number));
+
 // 4. Find Maximum Element
 // function findMax(arr: number[]): number {
 //     let max: number = arr[0];
@@ -39,6 +42,7 @@
 // }
 // let numbers: number[] = [10, 50, 20, 90, 30];
 // console.log("Maximum =", findMax(numbers));
+
 // 5.count even number
 // function countEven(arr: number[]): number {
 //     let count: number = 0;
@@ -51,6 +55,7 @@
 // }
 // let numbers: number[] = [10, 15, 20, 25, 30, 35];
 // console.log("Even Numbers =", countEven(numbers));
+
 //6.print even number
 // function printEven(arr: number[]): void {
 //     for (let i = 0; i < arr.length; i++) {
@@ -61,6 +66,7 @@
 // }
 // let numbers: number[] = [10, 15, 20, 25, 30, 35];
 // printEven(numbers);
+
 //7.search element
 // function searchElement(arr: number[], search: number): boolean {
 //     for (let i = 0; i < arr.length; i++) {
@@ -72,6 +78,7 @@
 // }
 // let numbers: number[] = [10, 20, 30, 40, 50];
 // console.log(searchElement(numbers, 30));
+
 //8. count positive number
 // function countPositive(arr: number[]): number {
 //     let count: number = 0;
@@ -84,6 +91,7 @@
 // }
 // let numbers: number[] = [-10, 20, -30, 40, 50];
 // console.log("Positive Numbers =", countPositive(numbers));
+
 //9.reverse array
 // function reverseArray(arr: number[]): number[] {
 //     return arr.reverse();
@@ -91,6 +99,7 @@
 // let numbers: number[] = [10, 20, 30, 40, 50];
 // console.log("Original Array:", numbers);
 // console.log("Reversed Array:", reverseArray(numbers));
+
 //10.factorial
 // function factorial(n) {
 //     let result = 1;
@@ -100,6 +109,7 @@
 //     return result;
 // }
 // console.log(factorial(5));
+
 //11. Find Sum of Even Numbers
 // function evenSum(arr: number[]): number {
 //     let sum = 0;
@@ -111,6 +121,7 @@
 //     return sum;
 // }
 // console.log(evenSum([10, 15, 20, 25, 30]));
+
 // 12. print nagitive number
 // function printNegative(arr: number[]) {
 //     for (let i = 0; i < arr.length; i++) {
@@ -121,6 +132,7 @@
 // }
 // let numbers: number[] = [10, -20, 30, -40, 50, -60];
 // printNegative(numbers);
+
 // 13. fibonacci
 // function fibonacci(n: number): void {
 //     let a = 0;
@@ -133,6 +145,7 @@
 //     }
 // }
 // fibonacci(10);
+
 // 14.Find Smallest Number
 // function smallest(arr: number[]): number {
 //     let min = arr[0];
@@ -144,6 +157,7 @@
 //     return min;
 // }
 // console.log(smallest([10, 50, 20, 80, 30]));
+
 // 15. search number
 // function search(arr: number[], value: number): boolean {
 //     for (let num of arr) {
