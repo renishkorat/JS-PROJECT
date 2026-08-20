45116c331760d4044560207f2bec3af7e1d957fc
renishkorat <koratrenish350@gmail.com>
2026-08-20 14:26:21 +0530

    Add PROJECT5

--- PROJECT2/pr2.js
+++ PROJECT2/pr2.js
@@ -1,170 +1,170 @@
-// 1. Print 1 to 10
-for (let i = 1; i <= 10; i++) {
-    console.log(i);
-}
+// // 1. Print 1 to 10
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
 
 
-// 2. Print 10 to 1
-let i = 10;
+// // 2. Print 10 to 1
+// let i = 10;
 
-while (i >= 1) {
-    console.log(i);
-    i--;
-}
-
-
-// 3. Print Odd Numbers
-let i = 1;
-
-while (i <= 20) {
-    console.log(i);
-    i += 2;
-}
+// while (i >= 1) {
+//     console.log(i);
+//     i--;
+// }
 
 
-// 4. Sum of 1 to 10
-let sum = 0;
+// // 3. Print Odd Numbers
+// let i = 1;
 
-for (let i = 1; i <= 10; i++) {
-    sum += i;
-}
-
-console.log(sum);
-
-
-// 5. Factorial of a Number
-let n = 5;
-let fact = 1;
-
-for (let i = 1; i <= n; i++) {
-    fact *= i;
-}
-
-console.log(fact);
+// while (i <= 20) {
+//     console.log(i);
+//     i += 2;
+// }
 
 
-// 6. Count Digits
-let num = 12345;
-let count = 0;
+// // 4. Sum of 1 to 10
+// let sum = 0;
 
-while (num > 0) {
-    count++;
-    num = Math.floor(num / 10);
-}
+// for (let i = 1; i <= 10; i++) {
+//     sum += i;
+// }
 
-console.log(count);
+// console.log(sum);
 
 
-// 7. Reverse Number
-let num = 1234;
-let rev = 0;
+// // 5. Factorial of a Number
+// let n = 5;
+// let fact = 1;
 
-while (num > 0) {
-    let rem = num % 10;
-    rev = rev * 10 + rem;
-    num = Math.floor(num / 10);
-}
+// for (let i = 1; i <= n; i++) {
+//     fact *= i;
+// }
 
-console.log(rev);
+// console.log(fact);
 
 
-// 8. Sum of Digits
-let num = 1234;
-let sum = 0;
+// // 6. Count Digits
+// let num = 12345;
+// let count = 0;
 
-while (num > 0) {
-    sum += num % 10;
-    num = Math.floor(num / 10);
-}
+// while (num > 0) {
+//     count++;
+//     num = Math.floor(num / 10);
+// }
 
-console.log(sum);
-
-
-// 9. Print Cubes
-let i = 1;
-
-do {
-    console.log(i * i * i);
-    i++;
-} while (i <= 10);
+// console.log(count);
 
 
-// 10. Fibonacci Series
-let a = 0;
-let b = 1;
+// // 7. Reverse Number
+// let num = 1234;
+// let rev = 0;
 
-for (let i = 1; i <= 10; i++) {
-    console.log(a);
+// while (num > 0) {
+//     let rem = num % 10;
+//     rev = rev * 10 + rem;
+//     num = Math.floor(num / 10);
+// }
 
-    let c = a + b;
-    a = b;
-    b = c;
-}
-
-
-//11. charactor check
-let ch = '@';
-
-if (
-    (ch >= 'A' && ch <= 'Z') ||
-    (ch >= 'a' && ch <= 'z')
-) {
-    console.log("The character is an alphabet");
-}
-else if (ch >= '0' && ch <= '9') {
-    console.log("The character is a digit");
-}
-else {
-    console.log("The character is a special character");
-}
+// console.log(rev);
 
 
-//12. square
-for (let i = 1; i <= 10; i++) {
-    console.log(i * i);
+// // 8. Sum of Digits
+// let num = 1234;
+// let sum = 0;
+
+// while (num > 0) {
+//     sum += num % 10;
+//     num = Math.floor(num / 10);
+// }
+
+// console.log(sum);
 
 
-}
+// // 9. Print Cubes
+// let i = 1;
+
+// do {
+//     console.log(i * i * i);
+//     i++;
+// } while (i <= 10);
 
 
-// 13. Check Prime Number
-let num = 17;
-let isPrime = true;
+// // 10. Fibonacci Series
+// let a = 0;
+// let b = 1;
 
-if (num <= 1) {
-    isPrime = false;
-} else {
-    for (let i = 2; i < num; i++) {
-        if (num % i === 0) {
-            isPrime = false;
-            break;
-        }
-    }
-}
+// for (let i = 1; i <= 10; i++) {
+//     console.log(a);
 
-if (isPrime) {
-    console.log("Prime");
-} else {
-    console.log("Not Prime");
-}
+//     let c = a + b;
+//     a = b;
+//     b = c;
+// }
 
 
-// 14. 6. Multiplication Table
-let num = 5;
-let i = 1;
+// //11. charactor check
+// let ch = '@';
 
-do {
-    console.log(num + " x " + i + " = " + (num * i));
-    i++;
-} while (i <= 10);
+// if (
+//     (ch >= 'A' && ch <= 'Z') ||
+//     (ch >= 'a' && ch <= 'z')
+// ) {
+//     console.log("The character is an alphabet");
+// }
+// else if (ch >= '0' && ch <= '9') {
+//     console.log("The character is a digit");
+// }
+// else {
+//     console.log("The character is a special character");
+// }
+
+
+// //12. square
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i * i);
+
+
+// }
+
+
+// // 13. Check Prime Number
+// let num = 17;
+// let isPrime = true;
+
+// if (num <= 1) {
+//     isPrime = false;
+// } else {
+//     for (let i = 2; i < num; i++) {
+//         if (num % i === 0) {
+//             isPrime = false;
+//             break;
+//         }
+//     }
+// }
+
+// if (isPrime) {
+//     console.log("Prime");
+// } else {
+//     console.log("Not Prime");
+// }
+
+
+// // 14. 6. Multiplication Table
+// let num = 5;
+// let i = 1;
+
+// do {
+//     console.log(num + " x " + i + " = " + (num * i));
+//     i++;
+// } while (i <= 10);
 
 
 // 15. Sum of Array Elements
-let numbers = [10, 20, 30, 40, 50];
-let sum = 0;
+// let numbers = [10, 20, 30, 40, 50];
+// let sum = 0;
 
-for (let i = 0; i < numbers.length; i++) {
-    sum += numbers[i];
-}
+// for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+// }
 
-console.log("Sum =", sum);
+// console.log("Sum =", sum);
